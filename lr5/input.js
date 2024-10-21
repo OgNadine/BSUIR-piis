19c8aad387a1ff78651f9bfe622e7359c0f98707
OgNadine <125201927+OgNadine@users.noreply.github.com>
2024-10-21 13:00:14 +0300

    add 6

--- lr5/input.js
+++ lr5/input.js
@@ -7,6 +7,8 @@ let offsetX, offsetY;
 let currentDiv = null;
 let currentAction = null;
 let previousTouchTime;
+let touchTimeStart;
+  
 
 targets.forEach(target => {
   let initialPosX = target.offsetLeft;
@@ -62,6 +64,14 @@ targets.forEach(target => {
     }
   });
 
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape" && currentDiv === target) {
+      resetPosition();
+    }
+  });
+
+  
+
   const resetPosition = () => {
     currentDiv = null;
     currentAction = null;
@@ -71,9 +81,48 @@ targets.forEach(target => {
     target.style.zIndex = '1';
   }
 
-  document.addEventListener("keydown", (event) => {
-    if (event.code === "Escape" && currentDiv === target) {
-      resetPosition();
+  const checkDoubleTouch = () => {
+    const currentTime = new Date().getTime();
+    const timeDiff = currentTime - previousTouchTime;
+    previousTouchTime = currentTime;
+    return timeDiff < 300;
+  }
+
+  target.addEventListener("touchstart", (e) => {
+    if (!currentDiv && currentAction !== 'touch_dblClick') {
+      currentDiv = target;
+      currentAction = 'touch_move';
+      offsetX = e.touches[0].clientX - target.offsetLeft;
+      offsetY = e.touches[0].clientY - target.offsetTop;
+      target.style.zIndex = targets.length;
+    }
+  });
+
+  document.addEventListener("touchstart", (e) => {
+    touchTimeStart = new Date().getTime();
+    if (e.touches.length > 1) resetPosition();
+  });
+
+  document.addEventListener("touchmove", (e) => {
+    if (currentAction) {
+      currentDiv.style.left = `${e.touches[0].clientX - offsetX}px`;
+      currentDiv.style.top = `${e.touches[0].clientY - offsetY}px`;
+    }
+  });
+
+  target.addEventListener("touchend", () => {
+    if (target === currentDiv && (currentAction === 'touch_move' || currentAction === 'touch_dblClick')) {
+      if (checkDoubleTouch() && new Date().getTime() - touchTimeStart > 100) {
+        currentAction = 'touch_dblClick';
+        target.style.backgroundColor = '#F4F4F4';
+        return;
+      }
+      currentDiv = null;
+      currentAction = null;
+      initialPosX = target.offsetLeft;
+      initialPosY = target.offsetTop;
+      target.style.backgroundColor = initialColor;
+      target.style.zIndex = '1';
     }
   });
 });
